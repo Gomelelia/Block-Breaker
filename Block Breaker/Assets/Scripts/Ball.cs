aa66a2a20c1776161b28af092d0cf7cc758f854a
Gomelelia <eliaxdoo96@gmail.com>
2021-06-09 22:34:53 +0300

    Block-Breaker

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,12 @@ public class Ball : MonoBehaviour
 {
     // config params   
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
     //state
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
@@ -16,6 +20,24 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlepos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlepos + paddleToBallVector;
