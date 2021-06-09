db787ddc15a43bacdb8be98dfd3d074a05a87e5f
Gomelelia <eliaxdoo96@gmail.com>
2021-06-09 10:52:06 +0300

    Block-Breaker
    Distance As a Vector2

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // config params   
+    [SerializeField] Paddle paddle1;
+
+    //state
+    Vector2 paddleToBallVector;
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlepos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlepos + paddleToBallVector;
+    }
+}
