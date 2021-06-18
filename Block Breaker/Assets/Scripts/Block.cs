23fcfe9815808aeccd006f4929feba493f0b9cc4
Gomelelia <eliaxdoo96@gmail.com>
2021-06-18 10:42:45 +0300

    Block Breaker
    Load next level

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -20,8 +20,13 @@ public class Block : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
-        Destroy(gameObject);
+        DestoryBlock();
     }
 
+    private void DestoryBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+        level.BlockDestroyed();
+    }
 }
--- Block Breaker/Assets/Scripts/Level.cs
+++ Block Breaker/Assets/Scripts/Level.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
+    // parameters
     [SerializeField] int breakableBloks; // Serialized for dubugging purposes
 
+    // cached reference
+    SceneLoader sceneloader;
 
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();  
+    }
     public void CountBreakableBlocs()
     {
         breakableBloks++;
     }
+
+    public void BlockDestroyed()
+    {
+        breakableBloks--;
+        if (breakableBloks <=0)
+        {
+            sceneloader.LoadNextScene();
+        }
+    }
+
 }
