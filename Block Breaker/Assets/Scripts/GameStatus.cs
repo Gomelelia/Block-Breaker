b7d8597382541aca570cca254594bd23cd482061
Gomelelia <eliaxdoo96@gmail.com>
2021-06-26 17:21:41 +0300

    Block-Breaker
    Display Player score

--- Block Breaker/Assets/Scripts/GameStatus.cs
+++ Block Breaker/Assets/Scripts/GameStatus.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameStatus : MonoBehaviour
 {
-    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f; 
-    // Start is called before the first frame update
-    void Start()
+    // config params
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    private void Start()
     {
-        
+        scoreText.text = currentScore.ToString();
     }
+
+
+    // Start is called before the first frame update
+
+    // state variables 
+    [SerializeField] int currentScore = 0;
 
     // Update is called once per frame
     void Update()
     {
         Time.timeScale = gameSpeed;
     }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
 }
+ 
